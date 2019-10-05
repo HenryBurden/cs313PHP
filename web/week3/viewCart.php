3083566e3ddf6d9a45e9f1bbfddb3c38d1fd4fa6
Henry Burden <bur15054@byui.edu>
2019-10-05 17:32:27 -0600

    base files added

---
+++ web/week3/viewCart.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Shopping Cart</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="shoppingCart.css">
+    <script src="https://kit.fontawesome.com/b765e10124.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <h1 class="head">
+            <div class="firstLetter">S</div>
+            <div class="otherLetters">ky</div>
+            <div class="firstLetter">S</div>
+            <div class="otherLetters">crapers</div>
+            <div class="firstLetter">I</div>
+            <div class="otherLetters">nc.</div>
+        </h1>
+        <ul>
+            <li><a href="browseItems.php">Browse Skyscrapers</a></li>
+            <li><a href="checkout.php">Checkout</a></li>
+            <li class="cart"><a href="viewCart.php">
+                <i class="fas fa-shopping-cart"></i></a>
+            </li>
+        </ul>
+    </header>
+
+    CART INFO HERE...
+
+</body>
+</html>
