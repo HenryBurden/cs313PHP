3c407434e083ac83fa65d161837c751e906fbb25
Henry Burden <bur15054@byui.edu>
2019-10-19 21:48:42 -0600

    Practice DB data Retrieval

--- web/week5/skullKingDBPract.php
+++ web/week5/skullKingDBPract.php
@@ -29,9 +29,9 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
   echo $row['player_name'];
   $statement2 = $db->query('SELECT bet, score FROM round WHERE player_id = $playerCount');
-    while ($row = $statement2->fetch(PDO::FETCH_ASSOC))
+    while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
     {
-        echo "<br>" . $row['bet'] . "  " . $row[score];
+        echo "<br>" . $row2['bet'] . "  " . $row2['score'];
     }
 }
 
