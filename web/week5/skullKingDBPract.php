d0193ec5bd631eaee61a4586c82551168da5bba5
Henry Burden <bur15054@byui.edu>
2019-10-19 21:39:29 -0600

    Practice DB data Retrieval

---
+++ web/week5/skullKingDBPract.php
@@ -0,0 +1,38 @@
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$statement = $db->query('SELECT player_name FROM player WHERE scorecard_id = 1');
+echo '<h1>ScoreCard</h1>';
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo $row['player_name'];
+}
+
+$statement = $db->query('SELECT bet, score FROM round WHERE player_id = 2');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo $row['bet'] . "  " . $row[score];
+}
+
+?>
