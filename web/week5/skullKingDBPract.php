e36ec883e89b42032087af7b9c6f0aab76cebd9e
Henry Burden <bur15054@byui.edu>
2019-10-26 20:11:51 -0600

    Text Input Testing

--- web/week5/skullKingDBPract.php
+++ web/week5/skullKingDBPract.php
@@ -28,12 +28,9 @@ $scores = array();
 $playerCount = 0;
 
 $statement = $db->query('SELECT player_name, player_id FROM player WHERE scorecard_id = 1');
-echo '<table><h1>ScoreCard</h1>';
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
   $players[$playerCount] = $row['player_name'];
-  echo '<tr><th colspan="2">'.$row['player_name'].'</th>';
-  echo '<tr><td>Bet</td><td>Score</td></tr>';
   $playerID = $row['player_id'];
   $statement2 = $db->query("SELECT bet, score FROM round WHERE player_id = $playerID");
   $roundCount = 0;
@@ -42,24 +39,32 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
       $bets[$playerCount][$roundCount] = $row2['bet'];
       $scores[$playerCount][$roundCount] = $row2['score'];
       $roundCount++;
-        echo '<tr><td><input type="text" id="bet" value="'.$row2['bet'].'"></td><td><input type="text" id="score" value="'.$row2['score'].'"></td></tr>';
     }
   $playerCount++;
 }
-echo '</table>';
+
+
+echo '<table><h1>ScoreCard</h1>';
+for($i = 0; $i < count($players); $i++) 
+{
+  //echo "<br>$players[$i]";
+  echo '<tr><th colspan="2">'.$players[$i].'</th>';
+  echo '<tr><td>Bet</td><td>Score</td></tr>';
+}
 
 for($i = 0; $i < count($players); $i++) 
 {
-  echo "<br>$players[$i]";
-  echo count($bets[$i]);
   for($j = 0; $j < count($bets[$i]); $j++)
   {
-    echo $bets[$i][$j].'<br>';
+    //echo $bets[$i][$j].'<br>';
+    echo '<tr><td><input type="text" id="bet" value="'.$bets[$i][$j].'"></td>';
   }
+
   for($j = 0; $j < count($scores[$i]); $j++)
   {
-    echo $scores[$i][$j].'<br>';
+    //echo $scores[$i][$j].'<br>';
+    echo '<td><input type="text" id="score" value="'.$scores[$i][$j].'"></td></tr>';
   }
 }
-
+echo '</table>';
 ?>
