aaac53ec8a802f9285c90dc507e24ff63b321a3e
Henry Burden <bur15054@byui.edu>
2019-10-26 22:36:55 -0600

    Text Input Testing

--- web/week5/skullKingDBPract.php
+++ web/week5/skullKingDBPract.php
@@ -69,7 +69,7 @@ for($rows = 0; $rows < 10; $rows++)
   for($columns = 0; $columns < 6; $columns++)
   {
     echo '<td><input class="bet" id="bet'.$columns.'R'.$rows.'" type="number" value="'.$bets[$columns][$rows].'"></td>';
-    echo '<td><input type="number" class="score" id="score'.$columns.'R'.$rows.'" onchange="updateDBScore();" value="'.$scores[$columns][$rows].'"></td>';
+    echo '<td><input type="number" class="score" id="score'.$columns.'R'.$rows.'" onchange="updateDBScore" value="'.$scores[$columns][$rows].'"></td>';
   }
   echo '</tr>';
 }
