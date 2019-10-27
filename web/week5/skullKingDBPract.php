fe87c884d6886005877a979730a5672140a054fb
Henry Burden <bur15054@byui.edu>
2019-10-26 19:53:54 -0600

    Text Input Testing

--- web/week5/skullKingDBPract.php
+++ web/week5/skullKingDBPract.php
@@ -54,10 +54,12 @@ for($i = 0; $i < count($players); $i++)
   echo count($bets[$i]);
   for($j = 0; $j < count($bets[$i]); $j++)
   {
+    echo $j;
     echo $bets[$i][$j];
   }
   for($j = 0; $j < count($scores[$i]); $j++)
   {
+    echo $j;
     echo $scores[$i][$j];
   }
 }
