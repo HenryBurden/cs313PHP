b38c0b8b26c394737cc4c3a438316d0b1eee442d
Henry Burden <bur15054@byui.edu>
2019-10-31 17:08:15 -0600

    Text Input Testing

---
+++ web/week5/skullKing.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Skull King</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="skullKing.css">
+
+</head>
+<body>
+    <h1>SKULLKING</h1>
+    <h3>Click button to create New Score Card</h3>
+    <button type="button" onclick="location.href='newScore.php'">Create</button>
+</body>
+</html>
