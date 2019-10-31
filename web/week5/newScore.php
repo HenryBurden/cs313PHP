084cf1a4be1141fcddbdca1504801cd34c0fb7c2
Henry Burden <bur15054@byui.edu>
2019-10-31 17:19:08 -0600

    Text Input Testing

---
+++ web/week5/newScore.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Connect to DB
+ */
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+//INSERT INTO scorecard DEFAULT VALUES; ONCE
+
+//INSERT INTO player DEFAULT VALUES; SIX TIMES
+//INSERT INTO round(round_number, player_id) VALUES($round_number, $player_id); 10 TIMES FOR EACH PLAYER
+
+
+try
+{
+   $query = "INSERT INTO scorecard DEFAULT VALUES RETURNING scorecard_id";
+   $statement = $db->prepare($query);
+   $id = $statement->execute();
+}
+catch (Exception $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+echo $id;
+
+
+?>
