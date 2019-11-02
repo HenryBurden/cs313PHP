b227dd32a6f3a33a633acdc53b1f5800490b6bcc
Henry Burden <bur15054@byui.edu>
2019-11-02 16:19:20 -0600

    Styling Fixes

--- web/week5/skullKing.php
+++ web/week5/skullKing.php
@@ -17,17 +17,17 @@
 <body>
     <div id="scoreCard">
         <h1>SKULLKING</h1>
-        <h3>New Score Card</h3>
+        <h2>New Score Card</h2>
         <button type="button" id="button" onclick="location.href='newScore.php'">Create</button>
 
         <h3> OR </h3>
-        <h3>
+        <h2>
             Existing Score Card #
             <form>
                 <input type="number" id="scorecard_id" name="scorecard_id">
                 <input type="button" value="Go" onclick="goToScoreCard()">
             </form>
-        </h3>
+        </h2>
     </div>
 </body>
 </html>
